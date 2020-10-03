eed137bf0f55b7d4c5c49f10c3349ef8ac24e9ca
mikolaj6r <mikolaj6r@gmail.com>
2020-10-03 19:44:35 +0200

    test

---
+++ lib/Component/message-parsers/Object.d.ts
@@ -0,0 +1,6 @@
+import * as React from 'react'
+declare const _default: React.SFC<import('emotion-theming/types/helper').AddOptionalTo<
+  any,
+  'theme'
+>>
+export default _default
---
+++ lib/definitions/Methods.d.ts
@@ -0,0 +1,14 @@
+declare const methods: string[]
+export default methods
+export declare type Methods =
+  | 'log'
+  | 'debug'
+  | 'info'
+  | 'warn'
+  | 'error'
+  | 'table'
+  | 'clear'
+  | 'time'
+  | 'timeEnd'
+  | 'count'
+  | 'assert'
